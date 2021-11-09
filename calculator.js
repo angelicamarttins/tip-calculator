6c507e5dac3afe9294e495a96ea5fb1aabce07ce
Angélica Martins <marttinsangelica@gmail.com>
2021-11-09 18:05:09 -0300

    Refactored the intNums function and started the validation in getPeople()

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ const bill = document.getElementById('bill');
 const custom = document.getElementById('custom');
 const buttonpc = document.querySelectorAll('.button__pc');
 const people = document.getElementById('people');
+const checkZero = document.getElementById('check--zero');
 const reset = document.getElementById('reset');
 
 // New variables
@@ -17,23 +18,27 @@ let total = 0;
 bill.addEventListener('input', getBill);
 buttonpc.forEach(element => element.addEventListener('click', getPercentage));
 custom.addEventListener('input', getCustom);
-people.addEventListener('input', amount);
+people.addEventListener('input', getPeople);
 
 // Functions 
 
 // Verifing if user entries in Number of People and Custom are interger numbers
 
 function intNums(num) {
-    let numRgx =/^\d[^\.]$/gm;
+    let numRgx = /^[0-9]*$/gm;
     return num.match(numRgx);
-}
+};
+
 // Setting the values of input 
 function getBill() {
     newBill = parseFloat(bill.value);
 };
 
 function getCustom() {
-    if (!intNums(custom.value)) {console.log('bla', custom.value)}
+    if (!intNums(custom.value)) {
+        custom.value = custom.value.substring(0, custom.value.length - 1);
+    };
+
     percentage = newBill * custom.value / 100;
 }
 
@@ -49,12 +54,23 @@ function getPercentage(event) {
     custom.value = "";
 }
 
+// Getting the number of people
+function getPeople() {
+    if (people.value == 0) {
+        checkZero.classList.add('check--zero');
+    }
+
+    if (!intNums(people.value)) {
+        people.value = people.value.substring(0, people.value.length - 1);
+    };
+
+    numPeople = people.value;
+}
 // Getting and calculating the values
 function amount() {
-    console.log(percentage)
-    numPeople = people.value
     tipAmount = percentage / numPeople;
     total = (newBill + percentage) / numPeople;
+
     res__tip.innerHTML = `$${tipAmount.toFixed(2)}`;
     res__total.innerHTML = `$${total.toFixed(2)}`;
 }
@@ -66,8 +82,6 @@ function resetBtn() {
     people.value = "";
     res__tip.innerHTML = `$0.00`;
     res__total.innerHTML = `$0.00`;
-}
+};
 
-// Ajustar regex no intNums para pegar casos que não sejam de inteiros. Acho que, então, o regex deve buscar apenas números e desprezar ponto ou vírgula. Assim, se for falso, o número inserido será real. 
-// Depois disso, pegar apenas os números iniciais e com ele fazer o cálculo. Talvez usando Math.ceil.
 // Depois fazer a verificação se o número de pessoas está com número negativo ou real.
