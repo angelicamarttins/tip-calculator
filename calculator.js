5a004f143dcc5dd44b048a85724b77c75a3b4813
Angélica Martins <marttinsangelica@gmail.com>
2021-11-09 12:04:52 -0300

    Refactored the getBill() e started the insNums()

--- calculator.js
+++ calculator.js
@@ -1,30 +1,57 @@
 // Getting elements from the page
 const bill = document.getElementById('bill');
 const custom = document.getElementById('custom');
-const buttonpc = document.getElementsByClassName('button__pc');
+const buttonpc = document.querySelectorAll('.button__pc');
 const people = document.getElementById('people');
 const reset = document.getElementById('reset');
 
 // New variables
 let newBill = 0;
 let percentage = 0;
+let buttonTips = 0;
 let numPeople = 0;
 let tipAmount = 0;
 let total = 0;
 
 // Event listener
-bill.addEventListener('input', getBillCustom);
-custom.addEventListener('input', getBillCustom);
+bill.addEventListener('input', getBill);
+buttonpc.forEach(element => element.addEventListener('click', getPercentage));
+custom.addEventListener('input', getCustom);
 people.addEventListener('input', amount);
 
 // Functions 
 
-function getBillCustom() {
+// Verifing if user entries in Number of People and Custom are interger numbers
+
+function intNums(num) {
+    let numRgx =/^\d[^\.]$/gm;
+    return num.match(numRgx);
+}
+// Setting the values of input 
+function getBill() {
     newBill = parseFloat(bill.value);
-    percentage = newBill * (custom.value / 100);
 };
 
+function getCustom() {
+    if (!intNums(custom.value)) {console.log('bla', custom.value)}
+    percentage = newBill * custom.value / 100;
+}
+
+// Getting the percentages of tips
+function getPercentage(event) {
+    let percRgx = /\d*[^%]/g;
+
+    buttonpc.forEach(e => {
+    if (event.target.innerHTML == e.innerHTML) 
+        percentage = newBill * parseFloat(e.innerHTML.match(percRgx)) / 100;
+    });
+
+    custom.value = "";
+}
+
+// Getting and calculating the values
 function amount() {
+    console.log(percentage)
     numPeople = people.value
     tipAmount = percentage / numPeople;
     total = (newBill + percentage) / numPeople;
@@ -32,6 +59,7 @@ function amount() {
     res__total.innerHTML = `$${total.toFixed(2)}`;
 }
 
+// Reset button
 function resetBtn() {
     bill.value = "";
     custom.value = "";
@@ -39,3 +67,7 @@ function resetBtn() {
     res__tip.innerHTML = `$0.00`;
     res__total.innerHTML = `$0.00`;
 }
+
+// Ajustar regex no intNums para pegar casos que não sejam de inteiros. Acho que, então, o regex deve buscar apenas números e desprezar ponto ou vírgula. Assim, se for falso, o número inserido será real. 
+// Depois disso, pegar apenas os números iniciais e com ele fazer o cálculo. Talvez usando Math.ceil.
+// Depois fazer a verificação se o número de pessoas está com número negativo ou real.
