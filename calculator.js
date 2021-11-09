764157d38db9a824aeeaa3cda4c62ab5b228269c
Angélica Martins <marttinsangelica@gmail.com>
2021-11-09 09:20:53 -0300

    finished the first calculations on inputs open to users

--- calculator.js
+++ calculator.js
@@ -1,17 +1,34 @@
-let bill = document.getElementById('bill');
-let custom = document.getElementById('custom');
+// Getting elements from the page
+const bill = document.getElementById('bill');
+const custom = document.getElementById('custom');
+const people = document.getElementById('people');
+
+// New variables
 let newBill = 0;
 let percentage = 0;
-let result = 0;
+let numPeople = 0;
+let tipAmount = 0;
+let total = 0;
 
+// Event listener
 bill.addEventListener('input', setBill);
 custom.addEventListener('input', getCustom);
+people.addEventListener('input', amount);
 
+// Functions 
 function setBill() {
-    newBill = bill.value;
+   return newBill = parseFloat(bill.value);
 };
 
 function getCustom() {
-    result = newBill * (custom.value / 100);
-    res__tip.innerHTML = `$${result}`
+    percentage = newBill * (custom.value / 100);
+    return percentage;
 };
+
+function amount() {
+    numPeople = people.value
+    tipAmount = percentage / numPeople;
+    total = (newBill + percentage) / numPeople;
+    res__tip.innerHTML = `$${tipAmount.toFixed(2)}`;
+    res__total.innerHTML = `$${total.toFixed(2)}`;
+}
